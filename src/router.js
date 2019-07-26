8e5d832b261019d220517521e654049c584680c0
Frah <alia0021@algonquinlive.com>
2019-07-26 13:33:54 -0400

    final

---
+++ src/router.js
@@ -0,0 +1,30 @@
+import Vue from "vue";
+import Router from "vue-router";
+import Products from "./views/ProductsList.vue";
+import SingleProduct from "./views/SingleProduct.vue";
+import ProductsInventory from "./views/ProductsInventory.vue";
+
+Vue.use(Router);
+
+export default new Router({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: Products
+    },
+    {
+      path: "/product/:id",
+      name: "single-product",
+      component: SingleProduct,
+      props: true
+    },
+    {
+      path: "/inventory",
+      name: "products-inventory",
+      component: ProductsInventory
+    }
+  ]
+});
---
+++ src/store.js
@@ -0,0 +1,80 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from "axios";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    products: []
+  },
+  mutations: {
+    SET_PRODUCTS(state, products) {
+      state.products = products;
+    },
+    PRODUCTS_SOLD(state, data) {
+      // update the product quantity in the state
+      const productIndex = this.getters.getProductIndex(data.product.id);
+      state.products[productIndex].quantity -= data.quantity;
+
+      // the line above is same as writing this
+      // state.products[productIndex].quantity = state.products[productIndex].quantity- data.quantity;
+
+      // Make API call to update the DB
+      axios
+        .put(
+          "https://alia0021-week-10.firebaseio.com/products.json",
+          state.products
+        )
+        .then(response => {
+          console.log("your data was updated " + response.status);
+        })
+        .catch(error => {
+          console.log("there was an issue saving your data " + error.response);
+        });
+    },
+    PRODUCT_QUANTITY(state, data) {
+      // get  the index of the product from the array of products
+      const productIndex = this.getters.getProductIndex(data.product.id);
+      // update the quantity of the product at the found index
+      state.products[productIndex].quantity = data.quantity;
+      // Make API call to update to DB
+      axios
+        .put(
+          "https://alia0021-week-10.firebaseio.com/products.json",
+          state.products
+        )
+        .then(response => {
+          console.log("your data was updated " + response.status);
+        })
+        .catch(error => {
+          console.log("there was an issue saving your data " + error.response);
+        });
+    }
+  },
+  actions: {
+    fetchData({ commit }) {
+      axios
+        .get("https://alia0021-week-10.firebaseio.com/products.json")
+        .then(response => {
+          console.log(response.data);
+          commit("SET_PRODUCTS", response.data);
+        });
+    },
+    buyProducts({ commit }, { quantity, product }) {
+      commit("PRODUCTS_SOLD", { quantity, product });
+    },
+    updateQuantity({ commit }, { quantity, product }) {
+      // commit a mutation to update the state, sending the payload as  {quantity, product }
+      commit("PRODUCT_QUANTITY", { quantity, product });
+    }
+  },
+  getters: {
+    getProductById: state => id => {
+      return state.products.find(product => product.id === id);
+    },
+    getProductIndex: state => id => {
+      return state.products.findIndex(product => product.id === id);
+    }
+  }
+});
